1f5199b9883223f56a1636a99ffd630c128cad7d
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2021-08-05 08:53:30 -0700

    Add files via upload

---
+++ SelectionSort.js
@@ -0,0 +1,55 @@
+/* 
+https://visualgo.net/en/sorting
+    
+Selection sort works by iterating through the list, finding the minimum
+value, and moving it to the beginning of the list. Then, ignoring the first
+position, which is now sorted, iterate through the list again to find the
+next minimum value and move it to index 1. This continues until all values
+are sorted.
+Unstable sort.
+
+Time Complexity
+    - Best: O(n^2) quadratic.
+    - Average: O(n^2) quadratic.
+    - Worst: O(n^2) quadratic.
+Space: O(1) constant.
+Selection sort is one of the slower sorts.
+- ideal for: pagination, where page 1 displays 10 records for example,
+    you run selection sort for 10 iterations only to display the first 10
+    sorted items.
+*/
+
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param   {Array<number>} nums
+ * @return  {Array<number>} The given array after being sorted.
+ */
+function selectionSort(nums) {
+    const len = nums.length;
+    let selectedIdx = 0;
+    let indexOfCurrMin = 0;
+
+    while( selectedIdx < len) {
+        for(var i = selectedIdx; i < len; i++) {
+            if(nums[i] < nums[indexOfCurrMin]) {
+                indexOfCurrMin = i;
+            }
+        }
+        if(nums[selectedIdx] !== nums[indexOfCurrMin]) {
+            [nums[selectedIdx], nums[indexOfCurrMin]] = [nums[indexOfCurrMin], nums[selectedIdx]];
+        }
+        selectedIdx += 1; 
+        indexOfCurrMin = selectedIdx;
+    }
+    return nums
+}
+console.log(selectionSort(numsRandomOrder));
+console.log(selectionSort(numsReversed));
